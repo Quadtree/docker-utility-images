2a0f1f3d2f2a0809de811b2a1099227bb435aa8b
Quadtree <quadtree@gmail.com>
2019-10-26 20:46:53 +0000

    Prefixes added

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-ls | grep -vP 'build.sh|README.md' | xargs -n1 -I{} docker build -t '{}' '{}/'
+ls | grep -vP 'build.sh|README.md' | xargs -n1 -I{} docker build -t 'quadtree2/{}' '{}/'
+ls | grep -vP 'build.sh|README.md' | xargs -n1 -I{} docker push 'quadtree2/{}'
