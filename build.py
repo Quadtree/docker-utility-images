7a1648b211ce9967a96b1ff187ca6b46d7dc12bd
Quadtree <quadtree@gmail.com>
2020-09-21 02:43:45 +0000

    Using threading

--- build.py
+++ build.py
@@ -5,6 +5,7 @@ import requests
 import re
 import subprocess
 import argparse
+import threading
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--only')
@@ -14,22 +15,33 @@ args = parser.parse_args()
 
 cur_path = os.path.realpath(os.path.dirname(__file__))
 
+def build_image(fn):
+    tag = 'latest'
+
+    try:
+        with open(os.path.join(cur_path, fn, 'version')) as f:
+            for l in f:
+                tag = l.strip()
+                break
+    except Exception: pass
+
+    additional = []
+    if args.no_cache: additional += ["--no-cache"]
+
+    if tag and tag != 'latest':
+        subprocess.run(['docker', 'build', '--build-arg', f'VERSION={tag}', '-t', 'quadtree2/' + fn + ':' + tag, fn] + additional)
+        subprocess.run(['docker', 'push', 'quadtree2/' + fn + ':' + tag])
+
+threads = []
+
 for fn in os.listdir(cur_path):
     if not args.only or fn == args.only:
         if os.path.isfile(os.path.join(cur_path, fn, 'Dockerfile')):
-            tag = 'latest'
+            new_thread = threading.Thread(target=lambda fn=fn: build_image(fn))
+            new_thread.start()
+            threads.append(new_thread)
 
-            try:
-                with open(os.path.join(cur_path, fn, 'version')) as f:
-                    for l in f:
-                        tag = l.strip()
-                        break
-            except Exception: pass
+for thread in threads:
+    thread.join()
 
-            additional = []
-            if args.no_cache: additional += ["--no-cache"]
 
-            if tag and tag != 'latest':
-                subprocess.run(['docker', 'build', '--build-arg', f'VERSION={tag}', '-t', 'quadtree2/' + fn + ':' + tag, fn] + additional)
-                subprocess.run(['docker', 'push', 'quadtree2/' + fn + ':' + tag])
-                
